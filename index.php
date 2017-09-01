016f5313b4b52a702065bd7369a0841c1874f4c6
Darkzerox <darknesscs2@gmail.com>
2017-09-01 18:34:21 +0700

    update data query

--- index.php
+++ index.php
@@ -9,27 +9,80 @@ Author URI:https://www.darkxee.com
 License: Plugin comes under GPL Licence.
 */
 
+
 wp_enqueue_script('datatable', plugins_url( '/js/jquery.dataTables.min.js' , __FILE__ ) , array( 'jquery' ));
-wp_register_style( 'datatables-css', plugins_url('css/jquery.dataTables.min.css', __FILE__) );
+wp_enqueue_script('datatable-respon', plugins_url( '/js/dataTables.responsive.min.js' , __FILE__ ) , array( 'jquery' ));
 wp_enqueue_script('custom-js', plugins_url( '/js/script.js' , __FILE__ ) , array( 'jquery' ));
 wp_localize_script( 'custom-js', 'darkxee', array( 'callurl' => admin_url( 'admin-ajax.php')));
 
 
+wp_enqueue_style( 'dzx-css', plugins_url('/css/dzx.css', __FILE__) );
+wp_enqueue_style( 'dataTables-css', plugins_url('/css/jquery.dataTables.min.css', __FILE__) );
+wp_enqueue_style( 'responsive-css', plugins_url('/css/responsive.dataTables.min.css', __FILE__) );
 
 
 
+
+
+/**
+ * [get_console console.log js]
+ * @param  [all] $data [data for console]
+ * @return [log]       [description]
+ */
 function get_console( $data ) {
 
     if ( is_array( $data ) )
-        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+      $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
     else
         $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
-
     echo $output;
 }
 
-
+//start html skeleton
 function show_form(){
+  echo '<div id="data-form" class="form-contentner">
+    <div class="form-group">
+      <label for="product_name">สินค้า</label>
+      <input type="text" name="product_name" id="product_name">
+    </div>
+    <div class="form-group">
+      <label for="size">ขนาดกระดาษ</label>
+      <input type="text" name="size" id="size">
+    </div>
+    <div class="form-group">
+      <label for="color">เทคนิคการพิมพ์</label>
+      <input type="text" name="color" id="color">
+    </div>
+    <div class="form-group">
+      <label for="page">จำนวนหน้า</label>
+      <input type="text" name="page" id="page">
+    </div>
+    <div class="form-group">
+      <label for="type">ชนิดการะดาษ</label>
+      <input type="text" name="type" id="type">
+    </div>
+    <div class="form-group">
+      <label for="quantity">จำนวน</label>
+      <input type="text" name="quantity" id="quantity">
+    </div>
+    <div class="form-group">
+      <label for="price_u">ราคา/หน่วย</label>
+      <input type="text" name="price_u" id="price_pu">
+    </div>
+    <div class="form-group">
+      <label for="price">ราคาทั้งหมด</label>
+      <input type="text" name="price" id="price">
+    </div>
+    <div class="form-group">
+      <label for="duration">ระยะเวลา</label>
+      <input type="text" name="duration" id="duration">
+    </div>
+    <div class="form-group full">
+      <input type="submit" name="Submit" id="data_submit">
+    </div>
+
+  </div>';
+  //table skeleton
   echo '<div id ="pim-data"></div>';
 }
 add_shortcode('dzx_data', 'show_form');
@@ -38,17 +91,35 @@ add_shortcode('dzx_data', 'show_form');
 
 function dzx_query(){
 
-	$name = $_POST['name'];
-	$size = $_POST['size'];
+  $name = $_POST['name'];
+  $size = $_POST['size'];
+  $color =$_POST['color'];
+  $page = $_POST['page'];
+  $type = $_POST['type'];
+  $quantity = $_POST['quantity'];
+  $price_pu = $_POST['price_pu'];
+  $price = $_POST['price'];
+  $duration = $_POST['duration'];
+
 
 
 
 	global $wpdb;
-  $get_data = $wpdb->get_results(
-    "SELECT * FROM pim123_product
-    WHERE name LIKE '$name'
-    AND size LIKE '$size'
-    AND quantity Like 500 " );
+
+  $cquery = 'SELECT * FROM pim123_product WHERE 1 ';
+  $cquery.= sql_candinate("name",$name);
+  $cquery.= sql_candinate("size",$size);
+  $cquery.= sql_candinate("color",$color);
+  $cquery.= sql_candinate("page",$page);
+  $cquery.= sql_candinate("type",$type);
+  $cquery.= sql_candinate("quantity",$quantity);
+  $cquery.= sql_candinate("price_pu",$price_pu);
+  $cquery.= sql_candinate("price",$price);
+  $cquery.= sql_candinate("size",$duration);
+
+
+
+  $get_data = $wpdb->get_results($cquery);
 
 	//echo $get_data;
 	$data = json_encode($get_data);
@@ -56,7 +127,18 @@ function dzx_query(){
   	die();
 
 }
+//use for loin user
 add_action('wp_ajax_dzx_query', 'dzx_query');
-add_action('wp_ajax_nopriv_dzx_query', 'dzx_dzx_query');
+//use for none login
+add_action('wp_ajax_nopriv_dzx_query', 'dzx_query');
+
+
+//return sql candinate
+function sql_candinate($cand ,$val){
+  if(!empty($val)){
+    $d = ' AND '.$cand.' LIKE "'.$val.'%"';
+  }
+  return $d ;
+}
 
 ?>
--- js/script.js
+++ js/script.js
@@ -1,73 +1,150 @@
 $ = jQuery;
 $(document).ready(function() {
 
-
-  $.ajax({
-    type: 'POST',
-    url: darkxee.callurl,
-    data: {
-      "action": "dzx_query", // use jquery get val
-      "name": 'กระดาษหัวจดหมาย',
-      "size": 'A4',
-
-    },
-    success: function(data) {
-
-      $('#pim-data').append(`
-        <table id="example" class="display" cellspacing="0" width="100%">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Size</th>
-              <th>Color</th>
-              <th>Page</th>
-              <th>Type</th>
-              <th>Quantity</th>
-              <th>Price/Unit</th>
-              <th>AllPrice</th>
-              <th>Duration</th>
-            </tr>
-          </thead>
-          <tfoot>
-            <tr>
-              <th>Name</th>
-              <th>Size</th>
-              <th>Color</th>
-              <th>Page</th>
-              <th>Type</th>
-              <th>Quantity</th>
-              <th>Price/Unit</th>
-              <th>AllPrice</th>
-              <th>Duration</th>
-            </tr>
-          </tfoot>
-        </table>
-        `);
-
-      data = JSON.parse(data);
-    //  console.log(data);
-
-      $('#example').DataTable({
-        "aaData": data,
-        "aoColumns": [
-          {"mDataProp": "name"},
-          {"mDataProp": "size"},
-          {"mDataProp": "color"},
-          {"mDataProp": "page"},
-          {"mDataProp": "type"},
-          {"mDataProp": "quantity"},
-          {"mDataProp": "price_pu"},
-          {"mDataProp": "price"},
-          {"mDataProp": "duration"}
-        ]
-
-      });
-
-      console.log(data);
-
-      //$('#btn-click').append(data);
-    }
+  $('#data_submit').on('click', function() {
+    update_table();
   });
+  $('#data-form .form-group').on('keyup', function() {
+    update_table();
+  });
+  //console.log('click');
+
+  //console.log($('#data-form #product_name').val());
+
+  /**
+   * [type ajax return sql data]
+   * @type {String}
+   */
+
+  function update_table() {
+    $.ajax({
+      type: 'POST',
+      url: darkxee.callurl,
+      data: {
+        //call php function
+        "action": "dzx_query",
+        //sent var data
+        "name": $('#data-form #product_name').val(),
+        "size": $('#data-form #size').val(),
+        "color": $('#data-form #color').val(),
+        "page": $('#data-form #page').val(),
+        "type": $('#data-form #type').val(),
+        "quantity": $('#data-form #quantity').val(),
+        "price_pu": $('#data-form #price_pu').val(),
+        "price": $('#data-form #price').val(),
+        "duration": $('#data-form #duration').val()
+
+
+      },
+      /**
+       * [return data from php function]
+       * @param  {[json]} data [sql data]
+       */
+      success: function(data) {
+
+console.log(  `
+  "name": ${$('#data-form #product_name').val()},
+  "size": ${$('#data-form #size').val()},
+  "color": ${$('#data-form #color').val()},
+  "page": ${$('#data-form #page').val()},
+  "type": ${$('#data-form #type').val()},
+  "quantity": ${$('#data-form #quantity').val()},
+  "price_pu": ${$('#data-form #price_pu').val()},
+  "price": ${$('#data-form #price').val()},
+  "duration": ${$('#data-form #duration').val()}
+  `);
+
+
+
+        $('#pim-data').html('').append(`
+            <table id="tabledata" class="display responsive nowrap">
+              <thead>
+                <tr>
+                  <th>สินค้า</th>
+                  <th>ขนาด</th>
+                  <th>สี</th>
+                  <th>จำนวนหน้า</th>
+                  <th>ชนิดกระดาษ</th>
+                  <th>จำนวน</th>
+                  <th>ราคา/ชื้น</th>
+                  <th>ราคาทั้งหมด</th>
+                  <th>ระยะเวลาผลิต</th>
+                </tr>
+              </thead>
+
+            </table>
+            `);
+
+
+        var columns = [{
+            "data": "name",
+            "responsivePriority": 1000
+          },
+          {
+            "data": "size",
+            "responsivePriority": 1
+          },
+          {
+            "data": "color",
+            "responsivePriority": 1
+          },
+          {
+            "data": "page",
+            "responsivePriority": 1
+          },
+          {
+            "data": "type",
+            "responsivePriority": 1
+          },
+          {
+            "data": "quantity",
+            "responsivePriority": 1002
+          },
+          {
+            "data": "price_pu",
+            "responsivePriority": 1
+          },
+          {
+            "data": "price",
+            "responsivePriority": 1003
+          },
+          {
+            "data": "duration",
+            "responsivePriority": 1000
+          },
+
+
+        ];
+        var columnDefs = [{
+            "responsivePriority": 1,
+            "targets": 2
+          },
+          {
+            "targets": -1,
+            "responsivePriority": 1,
+            "data": null,
+            "defaultContent": "<i class=\"fa fa-edit edit\" title=\"edit record\"></i>",
+            "orderable": false
+          }
+        ];
+
+        data = JSON.parse(data);
+        //console.log(data);
+
+        $('#tabledata').DataTable({
+          responsive: true,
+          searching: false,
+          "data": data,
+          "columns": columns,
+          "columnDefs": columnDefs,
+        });
+
+      }
+    });
+
+
+  }
+
 
 
 
