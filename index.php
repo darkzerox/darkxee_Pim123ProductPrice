71b85962b9d0f2d1dc8120f14d05f0c73d513dd5
Darkzerox <darknesscs2@gmail.com>
2017-09-15 19:15:50 +0700

    update new search

--- index.php
+++ index.php
@@ -41,58 +41,82 @@ function get_console( $data ) {
 //start html skeleton
 function show_form(){
   echo '<div id="data-form" class="form-contentner">
+  <div class="form-load">
+    <div>
+    <img src= "'.plugins_url('/images/load.gif', __FILE__).'"/>
+    </div>
+  </div>
           <div class="form-group search_product">
-            <label for="product_name">สินค้า</label>
-            <select class="input-text" name="product_name" id="product_name">
+            <label for="name">สินค้า</label>
+            <select class="input-text" name="name" id="name">
             </select>
           </div>
           <div id="table-field">
           </div>
-
+          <div class="form-group full">
+            <input type="submit" name="Submit" id="data_submit">
+          </div>
         </div>';
   //table skeleton
   echo '<div id ="pim-data"></div>';
 }
 add_shortcode('dzx_data', 'show_form');
 
-
+function getlist() {
+  $tabledata = $_POST['tabledata'];
+  getQuery('SELECT * FROM pim123_'.$tabledata.'');
+}
+add_action('wp_ajax_getlist', 'getlist');
+add_action('wp_ajax_nopriv_getlist', 'getlist');
 
 function dzx_query(){
 
   $name = $_POST['name'];
   $size = $_POST['size'];
   $color =$_POST['color'];
-  $page = $_POST['page'];
-  $type = $_POST['type'];
+  $page_type = $_POST['page_type'];
+  $paper_type = $_POST['paper_type'];
   $quantity = $_POST['quantity'];
-  $price_pu = $_POST['price_pu'];
+  $price_unit = $_POST['price_unit'];
   $price = $_POST['price'];
   $duration = $_POST['duration'];
+  $coating = $_POST['coating'];
+  $punch = $_POST['punch'];
+  $fold = $_POST['fold'];
+  $make_book = $_POST['make_book'];
+  $book_page = $_POST['book_page'];
+  $string = $_POST['string'];
+  $shape = $_POST['shape'];
+  $sticker_delivery = $_POST['sticker_delivery'];
+
 
   $cquery = 'SELECT * FROM pim123_'.$name.' WHERE 1 ';
-  $cquery.= sql_candinate("size",$size);
-  $cquery.= sql_candinate("color",$color);
-  $cquery.= sql_candinate("page",$page);
-  $cquery.= sql_candinate("type",$type);
-  $cquery.= sql_candinate("quantity",$quantity);
-  $cquery.= sql_candinate("price_pu",$price_pu);
-  $cquery.= sql_candinate("price",$price);
-  $cquery.= sql_candinate("size",$duration);
+  $cquery .= sql_candinate("size",$size);
+  $cquery .= sql_candinate("color",$color);
+  $cquery .= sql_candinate("page_type",$page_type);
+  $cquery .= sql_candinate("paper_type",$paper_type);
+  $cquery .= sql_candinate("quantity",$quantity);
+  $cquery .= sql_candinate("price_unit",$price_unit);
+  $cquery .= sql_candinate("price",$price);
+  $cquery .= sql_candinate("duration",$duration);
+  $cquery .= sql_candinate("coating",$coating);
+  $cquery .= sql_candinate("punch",$punch);
+  $cquery .= sql_candinate("fold",$fold);
+  $cquery .= sql_candinate("make_book",$make_book);
+  $cquery .= sql_candinate("book_page",$book_page);
+  $cquery .= sql_candinate("string",$string);
+  $cquery .= sql_candinate("shape",$shape);
+  $cquery .= sql_candinate("sticker_delivery",$sticker_delivery);
+
+
+
   getQuery($cquery);
+
+
+
 }
-//use for loin user
 add_action('wp_ajax_dzx_query', 'dzx_query');
-//use for none login
 add_action('wp_ajax_nopriv_dzx_query', 'dzx_query');
-
-
-
-function getlist() {
-  getQuery('SELECT * FROM pim123_products GROUP BY name ORDER BY name ASC');
-}
-add_action('wp_ajax_getlist', 'getlist');
-add_action('wp_ajax_nopriv_getlist', 'getlist');
-
 
 function getTable(){
   $tablename = $_POST['tablename'];
@@ -102,21 +126,17 @@ add_action('wp_ajax_getTable', 'getTable');
 add_action('wp_ajax_nopriv_getTable', 'getTable');
 
 
-
-
-
 //get data query
 function getQuery($query){
     global $wpdb;
     $get_data = $wpdb->get_results($query);
-    //echo $get_data;
   	echo json_encode($get_data);
     die();
 }
-//return sql candinate
 function sql_candinate($cand ,$val){
+  $d = "";
   if(!empty($val)){
-    $d = ' AND '.$cand.' LIKE "'.$val.'%"';
+    $d = ' AND '.$cand.' LIKE "'.$val.'"';
   }
   return $d ;
 }
--- js/script.js
+++ js/script.js
@@ -1,22 +1,210 @@
 $ = jQuery;
-$(document).ready(function() {
 
+//var includeTable = [];
+var excludeField = ['id', 'name', 'note', 'price_unit', 'price', 'duration', 'quantity'];
+
+//translaate
+var txtlist = {
+  'color': 'สี',
+  'size': 'จำนวน',
+  'page_type': 'จำนวนหน้าพิมพ์',
+  'paper_type': 'ประเภทกระดาษ',
+  'quantity': 'จำนวน',
+  'size': 'ขนาด',
+  'price_unit': 'ราคาต่อหน่วย',
+  'price': 'ราคารวม',
+  'duration': 'ระยะเวลาผลิต',
+  'coating': 'การเคลือบ',
+  'punch': 'การเจาะรู',
+  'fold': 'การพับ',
+  'make_book': 'การเข้าเล่ม',
+  'book_page': 'จำนวนหน้า',
+  'string': 'เชือก',
+  'shape': 'รูปทรงสติ๊กเกอร์',
+  'sticker_delivery': 'sticker_delivery',
+  'decorate': 'การตกแต่ง',
+
+
+
+};
+
+
+
+
+$(document).ready(function() {
   getProductList();
-  // getTable();
+
+  // update_table();
+
 
   $('#data_submit').on('click', function() {
-    getProductList();
+    $('.form-load').fadeIn();
     update_table();
-    getTable();
+  });
 
-  });
-  $('#data-form .form-group').on('keyup change', function() {
-    getProductList();
+  $('#data-form #name').change(function() {
+    $('.form-load').fadeIn();
     getTable();
-    update_table();
   });
+
 
 });
+
+function getProductList() {
+  $.ajax({
+    type: 'POST',
+    url: darkxee.callurl,
+    data: {
+      "action": "getlist",
+      "tabledata": "products",
+    },
+    success: function(data) {
+      data = JSON.parse(data).sort(name);
+
+      var listOption = "";
+
+      $.each(data, function(i, item) {
+        listOption += `<option value="${item.db_name}">${item.name}</option>`;
+      })
+      $('#name').append(listOption);
+
+      getTable();
+
+
+
+    }
+  });
+}
+
+function getOptionList() {
+  $.ajax({
+    type: 'POST',
+    url: darkxee.callurl,
+    data: {
+      "action": "getlist",
+      "tabledata": $('#name').val(),
+    },
+    success: function(data) {
+
+      $('.form-load').fadeOut();
+
+
+      data = JSON.parse(data);
+
+      var size = [];
+      var color = [];
+      var page_type = [];
+      var paper_type = [];
+      var quantity = [];
+      var price_unit = [];
+      var price = [];
+      var duration = [];
+      var coating = [];
+      var punch = [];
+      var fold = [];
+      var make_book = [];
+      var book_page = [];
+      var string = [];
+      var shape = [];
+      var sticker_delivery = [];
+      var decorate = [];
+
+
+
+      $.each(data, function(k, v) {
+        if (size.indexOf(v.size) == -1) {
+          size.push(v.size);
+        }
+        if (color.indexOf(v.color) == -1) {
+          color.push(v.color);
+        }
+        if (page_type.indexOf(v.page_type) == -1) {
+          page_type.push(v.page_type);
+        }
+        if (paper_type.indexOf(v.paper_type) == -1) {
+          paper_type.push(v.paper_type);
+        }
+        if (quantity.indexOf(v.quantity) == -1) {
+          quantity.push(v.quantity);
+        }
+        if (price_unit.indexOf(v.price_unit) == -1) {
+          price_unit.push(v.price_unit);
+        }
+        if (price.indexOf(v.price) == -1) {
+          price.push(v.price);
+        }
+        if (duration.indexOf(v.duration) == -1) {
+          duration.push(v.duration);
+        }
+        if (coating.indexOf(v.coating) == -1) {
+          coating.push(v.coating);
+        }
+        if (punch.indexOf(v.punch) == -1) {
+          punch.push(v.punch);
+        }
+        if (fold.indexOf(v.fold) == -1) {
+          fold.push(v.fold);
+        }
+        if (make_book.indexOf(v.make_book) == -1) {
+          make_book.push(v.make_book);
+        }
+        if (book_page.indexOf(v.book_page) == -1) {
+          book_page.push(v.book_page);
+        }
+        if (string.indexOf(v.string) == -1) {
+          string.push(v.string);
+        }
+        if (shape.indexOf(v.shape) == -1) {
+          shape.push(v.shape);
+        }
+        if (string.indexOf(v.string) == -1) {
+          string.push(v.string);
+        }
+        if (sticker_delivery.indexOf(v.sticker_delivery) == -1) {
+          sticker_delivery.push(v.sticker_delivery);
+        }
+        if (decorate.indexOf(v.decorate) == -1) {
+          decorate.push(v.decorate);
+        }
+
+      });
+      getOptionHtml(size, "#size");
+      getOptionHtml(color, "#color");
+      getOptionHtml(page_type, "#page_type");
+      getOptionHtml(paper_type, "#paper_type");
+      getOptionHtml(quantity, "#quantity");
+      getOptionHtml(price_unit, "#price_unit");
+      getOptionHtml(price, "#price");
+      getOptionHtml(coating, "#coating");
+      getOptionHtml(duration, "#duration");
+      getOptionHtml(punch, "#punch");
+      getOptionHtml(fold, "#fold");
+      getOptionHtml(make_book, "#make_book");
+      getOptionHtml(book_page, "#book_page");
+      getOptionHtml(string, "#string");
+      getOptionHtml(shape, "#shape");
+      getOptionHtml(string, "#string");
+      getOptionHtml(sticker_delivery, "#sticker_delivery");
+      getOptionHtml(decorate, "#decorate");
+
+
+    }
+  });
+}
+
+function getOptionHtml(data, id) {
+  data.sort();
+  listOption = "";
+  if (data.length > 0) {
+    $.each(data, function(i, item) {
+      listOption += `<option value="${item}">${item}</option>`;
+    })
+    $(id).append(listOption);
+  }
+
+}
+
+
 
 
 function getTable() {
@@ -25,32 +213,35 @@ function getTable() {
     url: darkxee.callurl,
     data: {
       'action': 'getTable',
-      'tablename': $('#product_name').val()
+      'tablename': $('#name').val()
 
     },
     success: function(data) {
-      console.log(data);
+
       data = JSON.parse(data);
+
       var formfield = "";
       $.each(data, function(i, item) {
 
-        if (item.COLUMN_NAME !== 'id' && item.COLUMN_NAME !== 'note' && item.COLUMN_NAME !== 'name') {
+        if (excludeField.indexOf(item.COLUMN_NAME) == -1) {
           formfield += `<div class="form-group">
-                          <label for="${item.COLUMN_NAME}">${item.COLUMN_NAME}</label>
+                          <label for="${item.COLUMN_NAME}">${__t(item.COLUMN_NAME)}</label>
                           <select class="input-text" type="text" name="${item.COLUMN_NAME}" id="${item.COLUMN_NAME}"></select>
                         </div>`
+        }
+
+        if (item.COLUMN_NAME !== 'id' && item.COLUMN_NAME !== 'note' && item.COLUMN_NAME !== 'name') {
+
 
         }
 
 
       })
-      formfield += `<div class="form-group full">
-        <input type="submit" name="Submit" id="data_submit">
-      </div>`;
+
       $('#table-field').html('');
       $('#table-field').append(formfield);
 
-
+      getOptionList();
 
     }
   });
@@ -65,15 +256,25 @@ function update_table() {
       //call php function
       "action": "dzx_query",
       //sent var data
-      "name": $('#data-form #product_name').val(),
+      "name": $('#data-form #name').val(),
       "size": $('#data-form #size').val(),
       "color": $('#data-form #color').val(),
-      "page": $('#data-form #page').val(),
-      "type": $('#data-form #type').val(),
+      "page_type": $('#data-form #page_type').val(),
+      "paper_type": $('#data-form #paper_type').val(),
       "quantity": $('#data-form #quantity').val(),
-      "price_pu": $('#data-form #price_pu').val(),
+      "price_unit": $('#data-form #price_unit').val(),
       "price": $('#data-form #price').val(),
-      "duration": $('#data-form #duration').val()
+      "duration": $('#data-form #duration').val(),
+      "coating": $('#data-form #coating').val(),
+      "punch": $('#data-form #punch').val(),
+      "fold": $('#data-form #fold').val(),
+      "make_book": $('#data-form #make_book').val(),
+      "book_page": $('#data-form #book_page').val(),
+      "string": $('#data-form #string').val(),
+      "shape": $('#data-form #shape').val(),
+      "sticker_delivery": $('#data-form #sticker_delivery').val(),
+      "decorate": $('#data-form #decorate').val(),
+
 
 
     },
@@ -82,89 +283,38 @@ function update_table() {
      * @param  {[json]} data [sql data]
      */
     success: function(data) {
+      data = JSON.parse(data);
+      console.log(data.length);
+      if (data.length > 0) {
+        console.log(data);
+        var resault = "";
+        $.each(data, function(k, v) {
+          var date = new Date();
+          date.setDate(date.getDate() + parseInt(v.duration));
 
+
+          resault += `<ul>
+            <li>จำนวน : ${v.quantity}</li>
+            <li>ราคาต่อชิ้น : ${v.price_unit}</li>
+            <li>ราคารวม : ${v.price}</li>
+            <li>วันที่ได้สินค้า : ${date.toDateString()}</li>
+            </ul>`;
+
+        });
+
+
+
+      } else {
+        resault = "ไม่พบข้อมูล";
+      }
 
       $('#pim-data').html('').append(`
-            <table id="tabledata" class="display responsive nowrap">
-              <thead>
-                <tr>
-                  <th>สินค้า</th>
-                  <th>ขนาด</th>
-                  <th>สี</th>
-                  <th>จำนวนหน้า</th>
-                  <th>ชนิดกระดาษ</th>
-                  <th>จำนวน</th>
-                  <th>ราคา/ชื้น</th>
-                  <th>ราคาทั้งหมด</th>
-                  <th>ระยะเวลาผลิต</th>
-                </tr>
-              </thead>
-
-            </table>
+        <ul>
+        ${resault}
+        </ul>
             `);
 
-
-      var columns = [{
-          "data": "name",
-          "responsivePriority": 1000
-        },
-        {
-          "data": "size",
-          "responsivePriority": 1
-        },
-        {
-          "data": "color",
-          "responsivePriority": 1
-        },
-        {
-          "data": "page",
-          "responsivePriority": 1
-        },
-        {
-          "data": "type",
-          "responsivePriority": 1
-        },
-        {
-          "data": "quantity",
-          "responsivePriority": 1002
-        },
-        {
-          "data": "price_pu",
-          "responsivePriority": 1
-        },
-        {
-          "data": "price",
-          "responsivePriority": 1003
-        },
-        {
-          "data": "duration",
-          "responsivePriority": 1000
-        },
-
-
-      ];
-      var columnDefs = [{
-          "responsivePriority": 1,
-          "targets": 2
-        },
-        {
-          "targets": -1,
-          "responsivePriority": 1,
-          "data": null,
-          "defaultContent": "<i class=\"fa fa-edit edit\" title=\"edit record\"></i>",
-          "orderable": false
-        }
-      ];
-
-      data = JSON.parse(data);
-
-      $('#tabledata').DataTable({
-        responsive: true,
-        searching: false,
-        "data": data,
-        "columns": columns,
-        "columnDefs": columnDefs,
-      });
+    $('.form-load').fadeOut();
 
     }
   });
@@ -172,29 +322,12 @@ function update_table() {
 
 }
 
-
-function getProductList() {
-  $.ajax({
-    type: 'POST',
-    url: darkxee.callurl,
-    data: {
-      "action": "getlist",
-    },
-    success: function(data) {
-      data = JSON.parse(data);
-      var listOption = "";
-
-      $.each(data, function(i, item) {
-        listOption += `<option value="${item.db_name}">${item.name}</option>`;
-      })
-      $('#product_name').append(listOption);
-      getTable();
-    }
-  });
-}
-
-
-function arrayRemoveDup(data) {
-  return data.filter((el, i, arr) => arr.indexOf(el) === i).sort();
+//trans
+function __t(txt) {
+  if (txt in txtlist) {
+    return txtlist[txt];
+  } else {
+    return txt;
+  }
 
 }
